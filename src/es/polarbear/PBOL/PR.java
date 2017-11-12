5dc3f3db3b2b38cd4325592d8cd5ef4db7924764
PolarBear <dualmacops@gmail.com>
2017-11-12 23:59:59 +0100

    Made methods public

--- src/es/polarbear/PBOL/PR.java
+++ src/es/polarbear/PBOL/PR.java
@@ -1,19 +1,19 @@
 package es.polarbear.PBOL;
 
 public class PR {
-    static void print() {
+    public static void print() {
         System.out.println();
     }
 
-    static void print(String s) {
+    public static void print(String s) {
         System.out.println(s);
     }
 
-    static void print(int s) {
+    public static void print(int s) {
         System.out.println(s);
     }
 
-    static void print(String[] strings) {
+    public static void print(String[] strings) {
         for (String x : strings) {
             System.out.println(x);
         }
