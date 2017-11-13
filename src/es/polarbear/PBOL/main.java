0a2b0029716e9cdeab6675151e09d6fa76ef6fc5
PolarBear <dualmacops@gmail.com>
2017-11-13 03:21:21 +0100

    Added Index Enumerator

--- src/es/polarbear/PBOL/main.java
+++ src/es/polarbear/PBOL/main.java
@@ -1,6 +1,16 @@
 package es.polarbear.PBOL;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class main {
     public static String split(String s, int start, int end) {
         return s.substring(start, end);
+    }
+
+    public static void indexEnumerator(ArrayList<String> sv) {
+        for (ListIterator<String> it = sv.listIterator(); it.hasNext(); ) {
+            System.out.println(it.nextIndex() + " " + it.next());
+        }
     }
 }
